e5870f422542089057d0e69628381b432ed3901b
Lyco Tatierra <frizth_tatierra@informatics.edu.ph>
2021-09-02 20:54:20 +0800

    Update restrict.js

--- restrict.js
+++ restrict.js
@@ -1,3 +1,20 @@
+/* 
+    This file is part of PHPCaptcha.
+
+    PHPCaptcha is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PHPCaptcha is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PHPCaptcha.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 var isCtrl = false;
 document.onkeyup=function(e)
 {
@@ -29,7 +46,7 @@ function mousehandler(e){
 document.oncontextmenu = mischandler;
 document.onmousedown = mousehandler;
 document.onmouseup = mousehandler;
-//select content code disable  alok goyal
+
 function killCopy(e){
     return false
 }
